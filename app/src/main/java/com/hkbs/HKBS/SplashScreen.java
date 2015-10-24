b677b247e46cf53223b0feba95837e064e8f0b85
itdanny33 <chow.danny@gmail.com>
2015-10-24 11:25:44 +0800

    New: 2016 Splash

--- app/src/main/java/com/hkbs/HKBS/SplashScreen.java
+++ app/src/main/java/com/hkbs/HKBS/SplashScreen.java
@@ -14,6 +14,7 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_splash);
         ImageView imageView = (ImageView) findViewById(R.id.imgLogo);
         if (CMain.IS_2016_OR_LATER) {
