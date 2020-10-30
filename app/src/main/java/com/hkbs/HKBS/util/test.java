3927f9042de027738ef2add59010648877572280
dchow <chow.danny@gmail.com>
2020-10-30 15:45:56 +0800

    Version 3.52 / 82 (Release)

---
+++ app/src/main/java/com/hkbs/HKBS/util/test.java
@@ -0,0 +1,4 @@
+package com.hkbs.HKBS.util;
+
+public class test {
+}
--- libSuperToasts/build/generated/source/buildConfig/debug/com/github/johnpersano/supertoasts/BuildConfig.java
+++ libSuperToasts/build/generated/source/buildConfig/debug/com/github/johnpersano/supertoasts/BuildConfig.java
@@ -6,13 +6,5 @@ package com.github.johnpersano.supertoasts;
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "com.github.johnpersano.supertoasts";
-  /**
-   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
-   */
-  @Deprecated
-  public static final String APPLICATION_ID = "com.github.johnpersano.supertoasts";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 11;
-  public static final String VERSION_NAME = "1.1";
 }
